9ff2bf3e29afe9cb06414dd7550acdb2e0d19359
Kenneth Yang <kjy5@uw.edu>
2025-03-19 19:51:53 -0700

    Share JSON

--- StoryboardXR/Views/StoryboardView.swift
+++ StoryboardXR/Views/StoryboardView.swift
@@ -21,38 +21,47 @@ struct StoryboardView: View {
     )
   }
   @State private var showSaveAlert = false
+  @State private var loadedScene: URL?
 
   var body: some View {
     VStack {
       Text("Storyboard View").font(.largeTitle)
 
-        Stepper(value: sceneNumberBinding, in: 1...1000) {
-          Text("Shot Number: \(appModel.sceneNumber)").font(.title)
-        }
+      Stepper(value: sceneNumberBinding, in: 1...1000) {
+        Text("Shot Number: \(appModel.sceneNumber)").font(.title)
+      }
       .padding()
-      
+
       HStack {
         Button("Save") {
           showSaveAlert = true
         }
         Button("Load") {
-          let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-          let filePath = documentsDirectory.first?.appendingPathComponent("Scene_\(appModel.sceneNumber).json")
-          
-          guard let filePath, FileManager.default.fileExists(atPath: filePath.path(percentEncoded: true)) else {
+          let documentsDirectory = FileManager.default.urls(
+            for: .documentDirectory, in: .userDomainMask)
+          let filePath = documentsDirectory.first?.appendingPathComponent(
+            "Scene_\(appModel.sceneNumber).json")
+
+          guard let filePath,
+            FileManager.default.fileExists(
+              atPath: filePath.path(percentEncoded: true))
+          else {
             print("Scene slot has not been used yet!")
             return
           }
-          
+
+          loadedScene = filePath
+
           do {
             let data = try Data(contentsOf: filePath)
-            appModel.shots = try JSONDecoder().decode([ShotModel].self, from: data)
+            appModel.shots = try JSONDecoder().decode(
+              [ShotModel].self, from: data)
           } catch {
             print("Failed to decode scene! \(error)")
           }
         }
       }
-      
+
       Divider()
 
       Button("Add Frame") {
@@ -60,6 +69,12 @@ struct StoryboardView: View {
       }
 
       Divider()
+
+      if let loadedScene = loadedScene {
+        ShareLink("Share Scene", item: loadedScene)
+        
+        Divider()
+      }
 
       Button("Switcher") {
         appModel.featureMode = .switcher
@@ -85,7 +100,7 @@ struct StoryboardView: View {
           print("Unable to encode! \(error)")
         }
       }
-      
+
       Button("Cancel", role: .cancel) {}
     } message: {
       Text("Saving a scene overwrites any scene with the same number.")
